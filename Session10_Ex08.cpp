7ea2caf54742035a11abb7f9993e95ef28c90122
KienVu102 <vudinhkien1002@gmail.com>
2024-12-03 12:01:31 +0700

    Kiên Vũ upload Session10_Ex08

---
+++ Session10_Ex08.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(){
+	int row, col, n = 0;
+	printf("Nhap so hang va so cot: ");
+	scanf("%d %d", &row, &col);
+    int arr[row][col], numbers[row*col];
+	
+	for(int i = 0; i < row; i++){
+		for(int j = 0; j < col; j++){
+			printf("Nhap phan tu arr[%d][%d]: ", i, j);
+			scanf("%d", &arr[i][j]);
+		}
+	}
+	n =0;
+	for(int i = 0; i < row; i++){
+		for(int j = 0; j < col; j++){
+			numbers[n] = arr[i][j];
+			n++;
+		}
+	}
+	for(int i = 0; i < col*row; i++){
+		for(int j = 0; j < col*row - i - 1; j++){
+			if(numbers[j] > numbers[j+1]){
+				int temp = numbers[j];
+				numbers[j] = numbers[j+1];
+				numbers[j+1] = temp;
+			}
+		}
+	}
+	n =0;
+	for(int i = 0; i < row; i++){
+		for(int j = 0; j < col; j++){
+			arr[j][i] = numbers[n];
+			n++;
+		}
+	}
+	for(int i = 0; i < row; i++){
+		for(int j = 0; j < col; j++){
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
